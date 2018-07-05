bf538fe3bb6170180132f375f1b8216f3bdfaa11
muthuhari <muthuhari009@gmail.com>
2018-07-06 00:08:39 +0530

    math added

---
+++ math.py
@@ -0,0 +1,18 @@
+class Math:
+
+	def __init__(x, y):
+		self.x = x
+		self.y = y
+
+	def add(self):
+		if x is not None and y is not None:
+			return self.x + self.y
+		
+	def sub(self):
+		return self.x - self.y
+		
+	def mul(self):
+		return self.x * self.y
+		
+	def div(self):
+		return self.x / self.y
